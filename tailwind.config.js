7c44900c330be84607422e225e5daab078e6be1c
Teslim <teslimni@gmail.com>
2021-04-21 09:57:11 +0100

    Laid out the search section and the nav section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      'sans': ['Arial', 'sans-serif'],
-     },
     extend: {},
   },
   variants: {
