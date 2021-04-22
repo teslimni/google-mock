1db6c38c186d0b5e7e535161aef5cfdfbff6b296
Teslim <teslimni@gmail.com>
2021-04-22 18:01:31 +0100

    Added a mobile first results page branch. Yet to work on the large responsive screen sizes

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 const search = document.querySelector('.search-box');
+const mobileSearch = document.querySelector('.mobile-search');
 const form = document.querySelector('.formOne');
 const inputFull = document.querySelector('.hide');
 const expand = document.querySelector('.droplist');
 const offers = document.querySelector('.search-tabs');
 const langs = document.querySelector('.search-langs');
 
-search.addEventListener('click', e => {
-    // search.style.display = 'none';
-    // inputFull.style.display = 'block';
-    form.classList.add('open');
-    search.classList.add('rounded-b-none', 'hover:shadow-none');
-    expand.classList.add('expanded');
-    expand.classList.remove('hide');
-    offers.classList.add('hide');
-    langs.classList.add('hide');
-}) 
+// search.addEventListener('click', e => {
+//     form.classList.add('open');
+//     search.classList.add('rounded-b-none', 'hover:shadow-none');
+//     mobileSearch.classList.add('hover:shadow-none');
+//     expand.classList.add('expanded');
+//     expand.classList.remove('hide');
+//     offers.classList.add('hide');
+//     langs.classList.add('hide');
+// }) 
+
+
+// Results Page Accordion
+const accordions = Array.from(document.querySelectorAll('.accordion'))
+accordions.forEach(accordion => {
+    // Find the accordion header
+    const accordionHeader = accordion.querySelector('.accordion__header')
+  
+    // Add event listener to the accordion header
+    accordionHeader.addEventListener('click', event => {
+  
+      // Toggle the is-open class
+      accordion.classList.toggle('is-open')
+    })
+  })
