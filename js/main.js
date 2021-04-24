d37fb8b7672d6e44c0fb87956b6d3572a5e137c0
Teslim <teslimni@gmail.com>
2021-04-24 19:30:10 +0100

    Done 96% with the homepage, page is responsive

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const search = document.querySelector('.search-box');
+const mobileSearch = document.querySelector('.mobile-search');
+const form = document.querySelector('.formOne');
+const inputFull = document.querySelector('.hide');
+const expand = document.querySelector('.droplist');
+const offers = document.querySelector('.search-tabs');
+const langs = document.querySelector('.search-langs');
+const footerOffer = document.querySelector('.scroller');
+const gOffer = document.querySelector('.g-Offer')
+
+search.addEventListener('click', e => {
+    form.classList.add('open');
+    search.classList.add('rounded-b-none', 'hover:shadow-none');
+    // mobileSearch.classList.add('hover:shadow-none');
+    expand.classList.add('expanded');
+    expand.classList.remove('hide');
+    offers.classList.add('hide');
+    langs.classList.add('hide');
+})
+
+mobileSearch.addEventListener('click', e => {
+    form.classList.add('open');
+    // search.classList.add('rounded-b-none', 'hover:shadow-none');
+    mobileSearch.classList.add('hover:shadow-none');
+    expand.classList.add('expanded');
+    expand.classList.remove('hide');
+    offers.classList.add('hide');
+    langs.classList.add('hide');
+})
+
+
+// Results Page Accordion
+const accordions = Array.from(document.querySelectorAll('.accordion'))
+accordions.forEach(accordion => {
+    // Find the accordion header
+    const accordionHeader = accordion.querySelector('.accordion__header')
+  
+    // Add event listener to the accordion header
+    accordionHeader.addEventListener('click', event => {
+  
+      // Toggle the is-open class
+      accordion.classList.toggle('is-open')
+    })
+})
+
+footerOffer.addEventListener('scroll', e => {
+    gOffer.classList.toggle('scrolled')
+})
--- js/results.js
+++ js/results.js
@@ -2,21 +2,22 @@ const search = document.querySelector('.search-box');
 const mobileSearch = document.querySelector('.mobile-search');
 const form = document.querySelector('.formOne');
 const inputFull = document.querySelector('.hide');
-const expand = document.querySelector('.droplist');
+const expand = document.querySelector('.mobile-droplist');
 const offers = document.querySelector('.search-tabs');
 const langs = document.querySelector('.search-langs');
 const footerOffer = document.querySelector('.scroller');
 const gOffer = document.querySelector('.g-Offer')
 
-// search.addEventListener('click', e => {
-//     form.classList.add('open');
-//     search.classList.add('rounded-b-none', 'hover:shadow-none');
-//     mobileSearch.classList.add('hover:shadow-none');
-//     expand.classList.add('expanded');
-//     expand.classList.remove('hide');
-//     offers.classList.add('hide');
-//     langs.classList.add('hide');
-// }) 
+
+mobileSearch.addEventListener('click', e => {
+    form.classList.add('open');
+    // search.classList.add('rounded-b-none', 'hover:shadow-none');
+    mobileSearch.classList.add('hover:shadow-none');
+    // expand.classList.add('expanded');
+    expand.classList.remove('hide');
+    offers.classList.add('hide');
+    langs.classList.add('hide');
+})
 
 
 // Results Page Accordion
